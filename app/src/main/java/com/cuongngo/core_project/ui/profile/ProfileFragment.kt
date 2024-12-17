d5af6a1a7ba2ff07bacea6fd96a9ba3937ab3dec
cuong.ngo <ngonhatcuonguit@gmail.com>
2024-12-17 11:31:11 +0700

    fix UI profile

--- app/src/main/java/com/cuongngo/core_project/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/cuongngo/core_project/ui/profile/ProfileFragment.kt
@@ -1,15 +1,14 @@
 package com.cuongngo.core_project.ui.profile
 
 import android.content.Intent
-import com.cuongngo.core_project.BuildConfig
 import com.cuongngo.core_project.R
 import com.cuongngo.core_project.base.fragment.BaseFragmentMVVM
 import com.cuongngo.core_project.base.viewmodel.kodeinViewModel
 import com.cuongngo.core_project.data.local.AppPreferences
+import com.cuongngo.core_project.data.local.AppPreferences.KEY_NAME
 import com.cuongngo.core_project.databinding.FragmentProfileBinding
 import com.cuongngo.core_project.ui.login.LoginMethodActivity
 import com.cuongngo.core_project.ui.sync_data.ActivitySyncData
-import com.cuongngo.core_project.utils.mark.setupTooltip
 
 class ProfileFragment : BaseFragmentMVVM<FragmentProfileBinding, ProfileViewModel>() {
 
@@ -19,84 +18,83 @@ class ProfileFragment : BaseFragmentMVVM<FragmentProfileBinding, ProfileViewMode
 
     override fun setUp() {
         with(binding){
-            tvName.text = "${AppPreferences.getUserInfo()?.first_name ?: ""} ${AppPreferences.getUserInfo()?.last_name ?: ""}"
-            AppPreferences.getUserInfo()?.email?.let {
-                tvDescriptionWelcome.text = "${AppPreferences.getUserInfo()?.position_name ?: ""}"
+            AppPreferences.getACBInfo(KEY_NAME).let {
+                tvName.text = it
             }
 
-            tvSyncData.setOnClickListener {
-//                gotoSyncData()
-            }
-            ivBgSyncData.setOnClickListener {
-//                gotoSyncData()
-            }
-            btnLogOut.setOnClickListener {
-//                clearUserData()
-//                gotoLoginMethod()
-            }
-            tvMember.setOnClickListener {
-                setupTooltip(
-                    requireContext(),
-                    "Feature Coming Soon",
-                    ivMember
-                )
-            }
-            tvChangePassword.setOnClickListener {
-                setupTooltip(
-                    requireContext(),
-                    "Feature Coming Soon",
-                    ivChangePassword
-                )
-            }
-            tvNotify.setOnClickListener {
-                setupTooltip(
-                    requireContext(),
-                    "Feature Coming Soon",
-                    ivNotify
-                )
-            }
-            tvLanguage.setOnClickListener {
-                setupTooltip(
-                    requireContext(),
-                    "Feature Coming Soon",
-                    ivLanguage
-                )
-            }
-            tvCountry.setOnClickListener {
-                setupTooltip(
-                    requireContext(),
-                    "Feature Coming Soon",
-                    ivCountry
-                )
-            }
-            tvClearCache.setOnClickListener {
-                setupTooltip(
-                    requireContext(),
-                    "Feature Coming Soon",
-                    ivClearCache
-                )
-            }
-            tvLegal.setOnClickListener {
-                setupTooltip(
-                    requireContext(),
-                    "Feature Coming Soon",
-                    ivLegal
-                )
-            }
-            tvHelp.setOnClickListener {
-                setupTooltip(
-                    requireContext(),
-                    "Feature Coming Soon",
-                    ivHelp
-                )
-            }
-            tvAbout.setOnClickListener {
-                setupTooltip(
-                    requireContext(),
-                    "Version ${BuildConfig.VERSION_NAME}\nContact: CuongNgo IT Digital Technical Specialist",
-                    ivAbout
-                )
-            }
+//            tvSyncData.setOnClickListener {
+////                gotoSyncData()
+//            }
+//            ivBgSyncData.setOnClickListener {
+////                gotoSyncData()
+//            }
+//            btnLogOut.setOnClickListener {
+////                clearUserData()
+////                gotoLoginMethod()
+//            }
+//            tvMember.setOnClickListener {
+//                setupTooltip(
+//                    requireContext(),
+//                    "Feature Coming Soon",
+//                    ivMember
+//                )
+//            }
+//            tvChangePassword.setOnClickListener {
+//                setupTooltip(
+//                    requireContext(),
+//                    "Feature Coming Soon",
+//                    ivChangePassword
+//                )
+//            }
+//            tvNotify.setOnClickListener {
+//                setupTooltip(
+//                    requireContext(),
+//                    "Feature Coming Soon",
+//                    ivNotify
+//                )
+//            }
+//            tvLanguage.setOnClickListener {
+//                setupTooltip(
+//                    requireContext(),
+//                    "Feature Coming Soon",
+//                    ivLanguage
+//                )
+//            }
+//            tvCountry.setOnClickListener {
+//                setupTooltip(
+//                    requireContext(),
+//                    "Feature Coming Soon",
+//                    ivCountry
+//                )
+//            }
+//            tvClearCache.setOnClickListener {
+//                setupTooltip(
+//                    requireContext(),
+//                    "Feature Coming Soon",
+//                    ivClearCache
+//                )
+//            }
+//            tvLegal.setOnClickListener {
+//                setupTooltip(
+//                    requireContext(),
+//                    "Feature Coming Soon",
+//                    ivLegal
+//                )
+//            }
+//            tvHelp.setOnClickListener {
+//                setupTooltip(
+//                    requireContext(),
+//                    "Feature Coming Soon",
+//                    ivHelp
+//                )
+//            }
+//            tvAbout.setOnClickListener {
+//                setupTooltip(
+//                    requireContext(),
+//                    "Version ${BuildConfig.VERSION_NAME}\nContact: CuongNgo IT Digital Technical Specialist",
+//                    ivAbout
+//                )
+//            }
 
         }
     }
@@ -118,6 +116,13 @@ class ProfileFragment : BaseFragmentMVVM<FragmentProfileBinding, ProfileViewMode
 
     companion object {
         val TAG = ProfileFragment::class.java.simpleName
+    }
+
+    override fun onResume() {
+        AppPreferences.getACBInfo(KEY_NAME).let {
+            binding.tvName.text = it
+        }
+        super.onResume()
     }
 
 }
